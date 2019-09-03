e7713f0021e2506c82795d8315a8bfd1842dbe04
BSDinis <baltasardinis@hotmail.com>
2019-09-03 02:59:41 +0100

    fixed patch-libressl; replace explicit_bzero with bzero, to circle linkage error (hack)

--- src/talos/patch_libressl.sh
+++ src/talos/patch_libressl.sh
@@ -8,3 +8,4 @@ cp enclaveshim/* ${LIBRESSL}/crypto/
 # patch the libressl files
 cd $LIBRESSL
 find ../talos/patch -mindepth 1 -maxdepth 1 -type f -name "*.patch" -print0 | xargs -0 -I {} patch -p0 -i {} 
+find . | grep .c$ | xargs grep "explicit_bzero" -r -l | xargs sed -i -e 's/explicit_bzero/bzero/g'
